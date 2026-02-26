bd6c8cf064ae2ec73c95268d0c6586888b005851
alvarocano21 <81304427+alvarocano21@users.noreply.github.com>
2026-02-26 10:19:48 +0100

    Delete 06_Contador_Palabras directory

--- 06_Contador_Palabras/contador_mejorado_V2.py
+++
@@ -1,8 +0,0 @@
-texto= "hola mundo -"
-
-print(texto.split())#cuenta palabras
-print(len(texto))#cuenta caracteres y escacios en blanco
-
-textoUsuario=input("introduce tenxto para contar: ")
-print(len(textoUsuario.split()), "palabras")
-print(len(textoUsuario), "caracteres")
